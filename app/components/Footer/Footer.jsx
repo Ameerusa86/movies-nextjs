a7189649f65bda4ffb1330f3932b71c25a7962e9
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-07-19 22:30:37 -0400

    Update Footer component styling

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -7,64 +7,72 @@ import {
   BsGoogle,
   BsGithub,
   BsYoutube,
+  BsBriefcase,
 } from "react-icons/bs";
+import "./Footer.css";
+
 const FooterComponent = () => {
   return (
     <>
       <footer className="myFooter mt-6">
-        <div className="mx-auto">
-          <div className="grid grid-cols-2 md:grid-cols-1 gap-4">
-            <div className="text-center">
-              <h3 className="text-xl font-bold">Follow Me : </h3>
-              <ul className="flex justify-center mt-4">
-                <li className="mx-2">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+            <div className="text-center md:text-left">
+              <h3 className="text-xl font-bold text-white">Follow Me</h3>
+              <ul className="flex justify-center md:justify-start mt-4 space-x-4">
+                <li>
                   <a
                     rel="noreferrer"
                     href="https://www.linkedin.com/in/ameer86/"
                     target="_blank"
+                    className="text-2xl text-white hover:text-blue-500 transition duration-300 transform hover:-translate-y-1"
                   >
-                    <BsLinkedin className="text-2xl" />
+                    <BsLinkedin />
                   </a>
                 </li>
-                <li className="mx-2">
+                {/* <li>
                   <a
                     rel="noreferrer"
                     href="https://stackoverflow.com/users/19499075/ameer"
                     target="_blank"
+                    className="text-2xl text-white hover:text-orange-500 transition duration-300 transform hover:-translate-y-1"
                   >
-                    <BsStackOverflow className="text-2xl" />
+                    <BsStackOverflow />
                   </a>
-                </li>
-                <li className="mx-2">
+                </li> */}
+                <li>
                   <a
                     rel="noreferrer"
-                    href="https://ameer-portfolio2023.netlify.app/"
+                    href="https://ameer-hasan-portfolio.vercel.app/"
                     target="_blank"
+                    className="text-2xl text-white hover:text-green-500 transition duration-300 transform hover:-translate-y-1"
                   >
-                    PORTFOLIO
+                    <BsBriefcase />
                   </a>
                 </li>
-                <li className="mx-2">
+                <li>
                   <a
                     rel="noreferrer"
                     href="https://github.com/Ameerusa86"
                     target="_blank"
+                    className="text-2xl text-white hover:text-gray-500 transition duration-300 transform hover:-translate-y-1"
                   >
-                    <BsGithub className="text-2xl" />
+                    <BsGithub />
                   </a>
                 </li>
-                <li className="mx-2">
+                <li>
                   <a
                     rel="noreferrer"
                     href="https://www.youtube.com/channel/UCoC2WTn5wlxcqGkbLbUH8-w"
                     target="_blank"
+                    className="text-2xl text-white hover:text-red-500 transition duration-300 transform hover:-translate-y-1"
                   >
-                    <BsYoutube className="text-2xl" />
+                    <BsYoutube />
                   </a>
                 </li>
               </ul>
             </div>
-            <div className="col-span-2 text-center">
+            <div className="text-center md:text-right">
               <div className="text-sm text-gray-500">
                 <small>
                   © Developed and Designed by{" "}
@@ -72,6 +80,7 @@ const FooterComponent = () => {
                     href="https://www.youtube.com/channel/UCoC2WTn5wlxcqGkbLbUH8-w"
                     target="_blank"
                     rel="noreferrer"
+                    className="text-white hover:text-amber-600 transition duration-300"
                   >
                     AMEER HASAN
                   </a>
--- app/components/Swiper/CastSwiper.jsx
+++ app/components/Swiper/CastSwiper.jsx
@@ -11,6 +11,9 @@ import Image from "next/image";
 
 const CastSwiper = ({ endpoint, apiKey, onDataChange }) => {
   const IMG_URL = "https://image.tmdb.org/t/p/original";
+  const PLACEHOLDER_MALE = "/../../public/Asset/male-placeholder.png";
+  const PLACEHOLDER_FEMALE = "/public/Asset/female-placeholder.jpg";
+  const PLACEHOLDER_UNKNOWN = "/public/Asset/unknown.png";
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -66,33 +69,46 @@ const CastSwiper = ({ endpoint, apiKey, onDataChange }) => {
         }}
         className="cast-swiper"
       >
-        {data.slice(0, 12).map((item, id) => (
-          <SwiperSlide key={id} className="swiper-slide">
-            <div className="relative group w-full h-full cursor-pointer">
-              <Image
-                src={IMG_URL + item.profile_path}
-                alt={item.name}
-                layout="responsive"
-                width={200}
-                height={300}
-                className="rounded-lg shadow-lg group-hover:shadow-2xl transition-shadow duration-300"
-                style={{
-                  objectFit: "cover",
-                }}
-              />
-              <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
-                <div className="absolute bottom-0 left-0 w-full p-4">
-                  <h3 className="text-sm md:text-base lg:text-lg font-semibold text-white">
-                    {item.name}
-                  </h3>
-                  <p className="text-xs md:text-sm text-gray-300">
-                    {item.character}
-                  </p>
+        {data.slice(0, 12).map((item, id) => {
+          let placeholder = PLACEHOLDER_UNKNOWN;
+          if (item.gender === 1) {
+            placeholder = PLACEHOLDER_FEMALE;
+          } else if (item.gender === 2) {
+            placeholder = PLACEHOLDER_MALE;
+          }
+
+          return (
+            <SwiperSlide key={id} className="swiper-slide">
+              <div className="relative group w-full h-full cursor-pointer">
+                <Image
+                  src={
+                    item.profile_path
+                      ? IMG_URL + item.profile_path
+                      : placeholder
+                  }
+                  alt={item.name}
+                  layout="responsive"
+                  width={200}
+                  height={300}
+                  className="rounded-lg shadow-lg group-hover:shadow-2xl transition-shadow duration-300"
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
+                  <div className="absolute bottom-0 left-0 w-full p-4">
+                    <h3 className="text-sm md:text-base lg:text-lg font-semibold text-white">
+                      {item.name}
+                    </h3>
+                    <p className="text-xs md:text-sm text-gray-300">
+                      {item.character}
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-        ))}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </>
   );
